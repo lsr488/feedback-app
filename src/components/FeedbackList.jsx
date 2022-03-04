5ed191d0800d4eb1ce9359898df68cef86f6cdd2
unknown <lsr488@gmail.com>
2022-03-04 08:59:57 -0500

    Initial commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,14 @@
+function FeedbackList({ feedback }) {
+	console.log(feedback)
+	
+	if(!feedback || feedback.length === 0) {
+		return <p>No Feedback Yet</p>
+	}
+	return <div className='feedback-list'>
+		{feedback.map((item) => {
+			<div>{item.rating}</div>
+		})}
+	</div>
+}
+
+export default FeedbackList
