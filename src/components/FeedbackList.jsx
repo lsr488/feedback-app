f51856505a9f2a4424c6c2c7c348e31bc213ecc7
unknown <lsr488@gmail.com>
2022-03-06 14:17:11 -0500

    Display data from props not hardcoded

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,13 @@
+import FeedbackItem from './FeedbackItem';
+
 function FeedbackList({ feedback }) {
-	console.log(feedback);
-	
 	if(!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>
 	}
 	return (
 		<div className='feedback-list'>
 			{feedback.map((item) => (
-				<div>{item.rating}</div>
+				<FeedbackItem key={item.id} item={item} />
 			))}
 		</div>
 		);
