81ef7bb0179da4ae42737887f6dc88608702db3c
unknown <lsr488@gmail.com>
2022-03-06 14:11:56 -0500

    Fixed wrong set of braces

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,16 @@
 function FeedbackList({ feedback }) {
-	console.log(feedback)
+	console.log(feedback);
 	
 	if(!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>
 	}
-	return <div className='feedback-list'>
-		{feedback.map((item) => {
-			<div>{item.rating}</div>
-		})}
-	</div>
+	return (
+		<div className='feedback-list'>
+			{feedback.map((item) => (
+				<div>{item.rating}</div>
+			))}
+		</div>
+		);
 }
 
-export default FeedbackList
+export default FeedbackList;
