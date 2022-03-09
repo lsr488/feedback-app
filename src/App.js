a4899d14a5f7e3b2a082fe8f095e54117b769d8b
unknown <lsr488@gmail.com>
2022-03-09 07:17:38 -0500

    Handling adding new feedback via form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import FeedbackForm from './components/FeedbackForm';
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
 
+	const addFeedback = (newFeedback) => {
+		console.log(newFeedback);
+	}
+
 	const deleteFeedback = (id) => {
 		// if(window.confirm('Are you sure you want to delete this item?')) {
 			setFeedback(feedback.filter((item) => item.id !== id));
@@ -19,7 +23,7 @@ function App() {
 		<>
 			<Header />
 			<div className="container">
-			<FeedbackForm />
+			<FeedbackForm handleAdd={addFeedback} />
 			<FeedbackStats feedback={feedback} />
 				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
