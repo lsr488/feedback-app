428045c4cda1b87dfa3d9bab7198d29d0ae80170
unknown <lsr488@gmail.com>
2022-03-09 08:29:55 -0500

    Use useContext to provide data for component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
-import { motion, AnimatePresence } from 'framer-motion'
-import PropTypes from 'prop-types'
+import {useContext} from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+	const {feedback} = useContext(FeedbackContext);
+
 	if(!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>
 	}
@@ -33,16 +36,6 @@ function FeedbackList({ feedback, handleDelete }) {
 	// 		))}
 	// 	</div>
 	// 	);
-}
-
-FeedbackList.propTypes = {
-	feedback: PropTypes.arrayOf(
-		PropTypes.shape({
-			id: PropTypes.number.isRequired, // change to string with uuidv4?
-			text: PropTypes.string.isRequired,
-			rating: PropTypes.number.isRequired
-		})
-	)
 }
 
 export default FeedbackList;
