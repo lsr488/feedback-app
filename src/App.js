37648a0725a825198b5ddf62fa97bb5229291f82
unknown <lsr488@gmail.com>
2022-03-09 08:40:36 -0500

    Use useContext to add new feedback

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink'
@@ -12,13 +9,6 @@ import {FeedbackProvider} from './context/FeedbackContext';
 
 // main app component
 function App() {
-	const [feedback, setFeedback] = useState(FeedbackData);
-
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	}
-
 	return (
 		<FeedbackProvider>
 			<Router>
@@ -27,7 +17,7 @@ function App() {
 					<Routes>
 						<Route exact path='/' element={
 							<>
-								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackForm />
 								<FeedbackStats />
 								<FeedbackList />
 							</>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import {createContext, useState} from 'react';
 
 const FeedbackContext = createContext();
@@ -11,6 +12,11 @@ export const FeedbackProvider = ({children}) => {
 		}
 	]);
 
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedback([newFeedback, ...feedback]);
+	}
+
 	const deleteFeedback = (id) => {
 		// if(window.confirm('Are you sure you want to delete this item?')) {
 			setFeedback(feedback.filter((item) => item.id !== id));
@@ -19,7 +25,8 @@ export const FeedbackProvider = ({children}) => {
 
 	return <FeedbackContext.Provider value={{
 		feedback,
-		deleteFeedback
+		deleteFeedback,
+		addFeedback
 	}}>
 		{children}
 	</FeedbackContext.Provider>
